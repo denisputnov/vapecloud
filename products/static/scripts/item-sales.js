fc924d3acdba028d48b79871e0317cdaed73a8b2
Denis Putnov <grnbows.connect@mail.ru>
2020-10-17 17:57:45 +0300

    bug-fix

--- products/static/scripts/item-sales.js
+++ products/static/scripts/item-sales.js
@@ -2,7 +2,7 @@ let sale = document.querySelector('.sale').textContent;
 let $defPrice = document.querySelector('.price');
 let $priceSpan = document.querySelector('#product-price');
 
-if (sale !== '0') {
+if (parseFloat(sale)) {
     let price = (defPrice - (defPrice * sale/100)).toFixed(2);
     $defPrice.insertAdjacentHTML('beforebegin', `<p class="item-def-price">${defPrice} руб.</p>`)
     $priceSpan.textContent = price;
