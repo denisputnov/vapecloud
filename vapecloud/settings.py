38b55d923b95db3a09eadf73a8580ec48391312b
syth0le <chdan565@gamil.com>
2020-10-18 12:47:42 +0300

    local

--- vapecloud/settings.py
+++ vapecloud/settings.py
@@ -123,8 +123,8 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Moscow'
+# TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
